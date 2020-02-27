e67b6ae3a6038567d6e8a775c466db0feed82d17
jaysin60 <jayantsingh75@gmail.com>
2020-02-27 19:17:13 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='grooming',
+version='0.1',
+description='safai is a mix of all the packages to clean the unstructured text',
+url='#',
+author='Jayant Singh',
+author_email='jayantsingh75@gmail.com',
+license='open source',
+# packages=['safai'],
+install_requires = ['nltk','word2number'],
+zip_safe=False)
